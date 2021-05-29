8fe791728817995cac2cfe110ed7b67344c84382
Lizardhbo <lizardhbo@gmail.com>
2021-05-29 16:16:49 +0200

    app.js

--- app.js
+++ app.js
@@ -6,3 +6,25 @@ if(menuBtn && menu) {
         menu.classList.toggle("show")
     })
 }
+
+// carousel
+
+const leftBtn = document.querySelector(".carousel .left")
+const rightBtn = document.querySelector(".carousel .right")
+const carouselSlides = document.querySelectorAll(".carousel .slide")
+
+if(carouselSlides && leftBtn && rightBtn) {
+    let counter = 0;
+
+    leftBtn.addEventListener("click", () => {
+        carouselSlides.forEach(slide => {
+            slide.classList.remove("show")
+        })
+        if(counter == 0) {
+            counter = carouselSlides.length -1
+        }  else {
+            counter--
+        }
+        carouselSlides[counter].classList.add("show")
+    })
+}
